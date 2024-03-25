4b7ac8db94c26e394c66c826a744ff249d2bf117
P <7398963+pawanpe@users.noreply.github.com>
2024-03-25 13:37:12 -0400

    Update repository.m for add-ons background jobs testing.

--- sample-releases-tbx-main/toolbox/repository.m
+++ sample-releases-tbx-main/toolbox/repository.m
@@ -1,4 +1,5 @@
 function repositoryMetadata = repository
     repositoryMetadata.name = "https://github.com/rks/sample-releases-tbx.git";
+    docsearch plot // add-ons background job consumer testing.
 end
 
