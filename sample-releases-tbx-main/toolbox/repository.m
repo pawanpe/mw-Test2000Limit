e0d33953b32265866797ba5b70caae44646f5800
P <7398963+pawanpe@users.noreply.github.com>
2024-05-22 13:40:24 -0400

    Update repository.m for default branch

--- sample-releases-tbx-main/toolbox/repository.m
+++ sample-releases-tbx-main/toolbox/repository.m
@@ -1,5 +1,6 @@
 function repositoryMetadata = repository
     repositoryMetadata.name = "https://github.com/rks/sample-releases-tbx.git";
     docsearch plot // add-ons background job consumer testing.
+    doc plot
 end
 
