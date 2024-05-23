64ab8c713a473bce4502f12ad9f0ab67e6447b9a
P <7398963+pawanpe@users.noreply.github.com>
2024-05-23 14:30:01 -0400

    Update repository.m

--- sample-releases-tbx-main/toolbox/repository.m
+++ sample-releases-tbx-main/toolbox/repository.m
@@ -2,5 +2,6 @@ function repositoryMetadata = repository
     repositoryMetadata.name = "https://github.com/rks/sample-releases-tbx.git";
     docsearch plot // add-ons background job consumer testing.
     doc plot
+    doc plot2
 end
 
